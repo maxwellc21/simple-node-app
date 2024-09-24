d8a74512f48e967f2d69d44e2942d9979a2e891d
maxwellc21 <maxiecletus@gmail.com>
2024-09-24 17:17:52 +1000

    Update to source file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Jenkins and Kubernetes!');
+  res.send('Hello, Jenkins and Kubernetes! Its working now');
 });
 
 app.listen(port, () => {
