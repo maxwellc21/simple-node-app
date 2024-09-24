4be395e0e0fb0385a740259ab7973a6375065f7f
maxwellc21 <maxiecletus@gmail.com>
2024-09-25 02:30:29 +1000

    new

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Jenkins .and Kubernetes! Its working now');
+  res.send('Hello, Jenkins neand Kubernetes! Its working now');
 });
 
 if (require.main === module) {
